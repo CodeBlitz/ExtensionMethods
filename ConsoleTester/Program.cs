ff023c7e2fdb2ce5c95a5fda5308e8c00a7fa47c
David Rupe <DavidFRupe@hotmail.com>
2019-08-25 16:05:44 -0700

    Add project files.

---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextValidation;
+
+
+namespace ConsoleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> testList = new List<string> { "a", " Z ", "Taco cat", "Taco, cat.", "Taco hat", "!a", "   mom", "Dad   .", " .," };
+
+            Console.WriteLine("Palindrome test using extension method.");
+
+            Console.WriteLine("Default lenght tests.");
+            foreach (string text in testList)
+            {
+                Console.WriteLine("[{0}] : {1}.", text, text.IsPalindrome());
+            }
+
+            Console.WriteLine("Minimum length of 1 passed as parameter.");
+            foreach (string text in testList)
+            {
+                Console.WriteLine("[{0}] : {1}.", text, text.IsPalindrome(1));
+            }
+
+            Console.WriteLine("Enter any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TextValidation/Palindrome.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace TextValidation
+{
+    public static class Palindrome
+    {
+        private const int MINIMUM_PALINDROME_LENGTH = 2;
+
+        /// <summary>
+        /// Palindrome evaluation of string.
+        /// </summary>
+        /// <remarks>
+        /// Spaces and punctuation ignored.        
+        /// </remarks>        
+        /// <returns>True if string is a palindrome.</returns>
+        public static bool IsPalindrome(this string text)
+        {
+            return text.IsPalindrome(MINIMUM_PALINDROME_LENGTH);
+        }
+
+        /// <summary>
+        /// Palindrome evaluation of string.
+        /// </summary>
+        /// <remarks>
+        /// Minimum 2 characters in length. Spaces and punctuation ignored.        
+        /// </remarks>        
+        /// <returns>True if string is a palindrome.</returns>
+        public static bool IsPalindrome(this string text, int minimumLength)
+        {
+            // Remove puncation.  
+            StringBuilder noPunctuationText = new StringBuilder();
+
+            foreach (char textCharacter in text)
+            {
+                if (Char.IsPunctuation(textCharacter) == false && Char.IsSeparator(textCharacter) == false)
+                {
+                    noPunctuationText.Append(textCharacter);
+                }
+            }
+
+            string cleaned = noPunctuationText.ToString();
+
+            if (cleaned.Length < minimumLength)
+            {
+                return false;
+            }
+
+            string reversed = new string(cleaned.Reverse().ToArray<char>());
+
+            if (reversed.Equals(cleaned, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
